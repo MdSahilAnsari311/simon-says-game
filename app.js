04bf95e8ee7973d212a43c08d9d40b2a58379b2c
MdSahilAnsari311 <sahilansari66965@gmail.com>
2025-11-29 21:37:39 +0530

    Refactor game logic in app.js: improve sequence generation and user input handling

---
+++ app.js
@@ -0,0 +1,90 @@
+// Simon-says-game
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+let h2 = document.querySelector("h2");
+let btns = ["yellow", "red", "purple", "green"];
+
+document.addEventListener("keypress", function() {
+    if(started == false){
+        console.log("game started");
+        started = true;
+
+        levelUp();
+    }
+}); 
+
+function gameFlash(btn){
+    btn.classList.add("gameflash");
+    setTimeout(function() {
+        btn.classList.remove("gameflash");
+    }, 250);
+}
+
+function levelUp(){
+   userSeq = [];
+   level++;
+   h2.innerText = `Level ${level}`;
+   
+   let randomIdx = Math.floor(Math.random() * 3);
+   let randomColor = btns[randomIdx];
+   let randomBtn = document.querySelector(`.${randomColor}`);
+   console.log(randomIdx);
+   console.log(randomColor);
+   console.log(randomBtn);
+   gameSeq.push(randomColor);
+   console.log(gameSeq);
+   gameFlash(randomBtn);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function() {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
+let maxScore = 0;
+function checkAns(idx){
+    
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    }else{
+        h2.innerHTML = `Game Over! Your score is <b>${level}</b> <br/> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        if(maxScore < level){
+            maxScore = level;
+            h2.innerHTML = `Game Over! Your new highest score is ${maxScore} <br/> Press any key to start.`
+        }
+        reset();
+    }
+}
+
+function btnPress(){
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
